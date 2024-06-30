4f428377858839d22848e3c31baaadecbb29ad03
Belal <119034702+BelalKhaleel@users.noreply.github.com>
2024-06-30 14:57:09 +0300

    Make backspace key work

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const log = (...args) => {
   return console.log(...args);
-}
+};
 
-let previousNumber = '';
-let nextNumber = '';
-let operator = '';
+let previousNumber = "";
+let nextNumber = "";
+let operator = "";
+let currentNumber = "";
 
-const screen = document.querySelector('.screen');
-
-const buttons = document.querySelector('.buttons');
-
-buttons.addEventListener('click', populateDisplay);
+const screen = document.querySelector(".screen");
+const buttons = document.querySelector(".buttons");
+const operands = buttons.querySelectorAll(".operand");
+const decimalPoint = buttons.querySelector(".decimal-point");
 
 const calculator = (function () {
   const add = (a, b) => a + b;
@@ -22,49 +22,69 @@ const calculator = (function () {
 })();
 
 function operate(previousNumber, nextNumber, operator) {
-  switch(operator) {
-    case '+':
+  switch (operator) {
+    case "+":
       return calculator.add(previousNumber, nextNumber);
-    case '-':
+    case "-":
       return calculator.sub(previousNumber, nextNumber);
-    case '×':
+    case "×":
       return calculator.mul(previousNumber, nextNumber);
-    case '÷':
+    case "÷":
       return calculator.div(previousNumber, nextNumber);
-    case '%':
+    case "%":
       return calculator.mod(previousNumber, nextNumber);
   }
 }
 
-let currentNumber = '';
+function disableButtons() {
+  if (currentNumber.length > 13) {
+    operands.forEach((operand) => {
+      operand.disabled = true;
+    });
+    decimalPoint.disabled = true;
+  }
+}
+
+function enableButtons() {
+  operands.forEach((operand) => {
+    operand.disabled = false;
+  });
+  decimalPoint.disabled = false;
+}
 
 function populateDisplay(e) {
   let buttonClass = e.target.classList;
-  if (
-    buttonClass.contains('operand') 
-    || 
-    buttonClass.contains('decimal')
-  ) {
+  if (buttonClass.contains("operand") || buttonClass.contains("decimal-point")) {
     currentNumber += e.target.value;
+    disableButtons();
     screen.textContent = currentNumber;
   }
-  if (buttonClass.contains('operator')) {
+  if (buttonClass.contains("operator")) {
     previousNumber = Number(currentNumber);
-    currentNumber = '';
+    currentNumber = "";
     operator = e.target.value;
+    enableButtons();
   }
-  if (buttonClass.contains('equal')) {
+  if (buttonClass.contains("equal")) {
     nextNumber = Number(currentNumber);
     currentNumber = operate(previousNumber, nextNumber, operator);
+    if (currentNumber.length > 13) {
+      screen.textContent = String(Math.round(currentNumber));
+    } else {
+      screen.textContent = currentNumber;
+    }
+  }
+  if (buttonClass.contains("clear")) {
+    previousNumber = "";
+    nextNumber = "";
+    currentNumber = "";
+    screen.textContent = "";
+    enableButtons();
+  }
+  if (buttonClass.contains("backspace")) {
+    currentNumber = currentNumber.split("").slice(0, -1).join("");
     screen.textContent = currentNumber;
   }
-  if (buttonClass.contains('clear')) {
-    previousNumber = '';
-    nextNumber = '';
-    screen.textContent = '';
-  }
-  // if (buttonClass.contains('backspace')) {
-  //     log(screen.textContent);
-  //     screen.textContent.split('').pop().join('');
-  // }
 }
+
+buttons.addEventListener("click", populateDisplay);
